0063a7437e45257d91b225eb40131e32f90d9a60
worione <hyorax@gmail.com>
2020-09-24 10:43:06 +0300

    Game Upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+var button = document.getElementsByTagName('button');
+
+var random = Math.floor(Math.random() * 3);
+
+button[random].setAttribute("rightoption", true);
+
+for (var i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', function () {
+        var correct = this.getAttribute("rightoption");
+        if (correct == "true") {
+            alert("True!")
+        } else {
+            alert("Wrong!");
+        }
+    });
+}
